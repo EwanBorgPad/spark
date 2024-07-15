c7ddef2e777541dc36131261a953cba9c046f8ec
Shug Ninx <174782902+ShugNinx@users.noreply.github.com>
2024-07-15 12:29:23 +0200

    draft onchain file structure

---
+++ onchain/borgpad/src/lib.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+use solana_security_txt::security_txt;
+
+declare_id!("awerLVSjzcEWgRTK7WwbRZFnkhbUNHG5dwNMvY2a9JK");
+
+pub mod errors;
+pub mod events;
+
+#[program]
+pub mod borgpad {
+    use super::*; 
+
+}
