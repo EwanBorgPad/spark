071543d710511a550693f8c540ca390f65f31221
Shug Ninx <174782902+ShugNinx@users.noreply.github.com>
2024-07-16 15:10:09 +0200

    config state pda and lbp state pda strucutre and initializer

--- onchain/borgpad/src/instructions/initialize_lbp.rs
+++ onchain/borgpad/src/instructions/initialize_lbp.rs
@@ -1,13 +1,41 @@
 use anchor_lang::prelude::*;
-use solana_program::log::sol_log;
+use crate::errors::ErrorCode;
+use crate::state::config::*;
+use crate::state::lbp::*;
 
 #[derive(Accounts)]
-pub struct InitializeLBP<'info> {
-    pub treasurer: Signer<'info>,
+#[instruction(uid: u64)]
+pub struct InitializeLbp<'info> {
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        init,
+        space = Lbp::LEN,
+        seeds = [
+            b"lbp".as_ref(),
+            &uid.to_le_bytes()
+        ],
+        bump,
+        payer = admin
+    )]
+    pub lbp: Account<'info, Lbp>,
+
+    #[account(
+        mut,
+        constraint = config.admin_authority == admin.key() @ ErrorCode::NotAdmin
+    )]
+    pub admin: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
-pub fn handler(_ctx: Context<InitializeLBP>) -> Result<()> {
-    sol_log("Init LBP");    
+pub fn handler(ctx: Context<InitializeLbp>, lbp_static_data: LbpStaticData) -> Result<()> {
+    let lbp = Lbp { static_data: lbp_static_data, dynamic_data: LbpDynamicData::default() };
+    ctx.accounts.lbp.set_inner(lbp);
 
     Ok(())
 }
--- onchain/borgpad/src/lib.rs
+++ onchain/borgpad/src/lib.rs
@@ -1,18 +1,32 @@
 use anchor_lang::prelude::*;
+use crate::instructions::initialize::*;
 use crate::instructions::initialize_lbp::*;
+use crate::state::lbp::LbpStaticData;
 use solana_security_txt::security_txt;
 
 pub mod instructions;
 pub mod errors;
 pub mod events;
+pub mod state;
 
 declare_id!("awerLVSjzcEWgRTK7WwbRZFnkhbUNHG5dwNMvY2a9JK");
 
 #[program]
 pub mod borgpad {
-    use super::*; 
+    use super::*;
 
-    pub fn initialize_lbp(ctx: Context<InitializeLBP>) -> Result<()> {
-        return instructions::initialize_lbp::handler(ctx);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        admin_authority: Pubkey,
+        whitelist_authority: Pubkey
+    ) -> Result<()> {
+        return instructions::initialize::handler(ctx, admin_authority, whitelist_authority);
+    }
+
+    pub fn initialize_lbp(
+        ctx: Context<InitializeLbp>,
+        lbp_static_data: LbpStaticData
+    ) -> Result<()> {
+        return instructions::initialize_lbp::handler(ctx, lbp_static_data);
     }
 }
