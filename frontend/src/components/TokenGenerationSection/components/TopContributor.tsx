47740cc858a75ce95c04f70d5b29e9ff1df2b8c5
Vanja <vanja.jaksic@codetri.be>
2024-09-27 14:05:06 +0200

    fix: resolve bugs for top contributor, Img fallback, LiveNowExchange max length

--- frontend/src/components/TokenGenerationSection/components/TopContributor.tsx
+++ frontend/src/components/TokenGenerationSection/components/TopContributor.tsx
@@ -5,8 +5,8 @@ const TopContributor = () => {
 
   return (
     <div className="w-full rounded-lg bg-gradient-to-t from-brand-primary/50 to-brand-primary/15 p-[1px]">
-      <div className="bg-top-contributor relative flex items-center gap-4 rounded-[7px] bg-secondary object-cover px-4 py-3">
-        <div className="bg-top-contributor absolute bottom-0 left-0 -z-[1] h-[83px] w-full"></div>
+      <div className=" relative flex items-center gap-4 rounded-[7px] bg-secondary bg-top-contributor bg-bottom bg-no-repeat object-cover px-4 py-3 ">
+        <div className="absolute bottom-0 left-0 -z-[1] h-[83px] w-full "></div>
         <Icon icon="SvgTrophy" className="text-2xl text-brand-primary" />
         <div className="flex flex-col items-start">
           <span className="font-medium">{`You are a top ${10}% contributor`}</span>
