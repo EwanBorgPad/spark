922ca6120f3b9bb491cce13f8b6b0963433b5ccc
milan.miljus <milan.miljus@codetri.be>
2024-11-19 16:20:26 +0100

    docs: export.bash

--- frontend/scripts/export.bash
+++ frontend/scripts/export.bash
@@ -1,6 +1,7 @@
-#npx wrangler d1 execute borgpad-production-database --remote --command "SELECT
-#    address,
-#    json -> 'investmentIntent' -> 'solana-id' ->> 'amount' AS investment_interest_amount,
-#    json -> 'investmentIntent' -> 'solana-id' ->> 'providedAt' AS investment_interest_provided_at,
-#    json -> 'termsOfUse' ->> 'acceptedAt' AS terms_of_use_accepted_at
-#FROM user;" &> "export_$(date -u +%Y%m%d_%H%M%S).json"
+npx wrangler d1 execute borgpad-production-database --remote --command "SELECT
+  address,
+  json -> 'investmentIntent' -> 'solana-id' ->> 'amount' AS investment_interest_amount,
+  json -> 'investmentIntent' -> 'solana-id' ->> 'providedAt' AS investment_interest_provided_at,
+  json -> 'termsOfUse' ->> 'countryOfOrigin' AS terms_of_use_country_of_origin,
+  json -> 'termsOfUse' ->> 'acceptedAt' AS terms_of_use_accepted_at
+FROM user;" &> "export_$(date -u +%Y%m%d_%H%M%S).json"
