0b2e4c7811ba24ef14e3dfe2cc1b5c2360b8899b
Vanja <vanja.jaksic@codetri.be>
2024-11-21 14:37:50 +0100

    feat: naming update

--- frontend/src/utils/getCurrentTgeEvent.ts
+++ frontend/src/utils/getCurrentTgeEvent.ts
@@ -1,7 +1,7 @@
 import { ExpandedTimelineEventType } from "@/components/Timeline/Timeline"
 import { isBefore } from "date-fns/isBefore"
 
-const upcomingStatusTemplate: ExpandedTimelineEventType = {
+const upcomingEventTemplate: ExpandedTimelineEventType = {
   label: "Registration not opened yet",
   id: "UPCOMING",
   idRank: 1,
@@ -9,14 +9,14 @@ const upcomingStatusTemplate: ExpandedTimelineEventType = {
   nextEventDate: null,
 }
 
-const generateUpcomingStatus = (whitelistStartDate: Date | null) => {
-  const upcomingStatus = Object.assign(upcomingStatusTemplate, { nextEventDate: whitelistStartDate })
+const generateUpcomingEvent = (whitelistStartDate: Date | null) => {
+  const upcomingStatus = Object.assign(upcomingEventTemplate, { nextEventDate: whitelistStartDate })
   return upcomingStatus
 }
 
 export const getCurrentTgeEvent = (timeline: ExpandedTimelineEventType[]) => {
   const currentMoment = new Date()
-  const status = timeline.find((event) => {
+  const activeEvent = timeline.find((event) => {
     const hasEventStarted = event.date && !isBefore(currentMoment, event.date)
     const isEventFinished = event.date && isBefore(event.date, currentMoment)
     if (!hasEventStarted) return false
@@ -26,10 +26,10 @@ export const getCurrentTgeEvent = (timeline: ExpandedTimelineEventType[]) => {
     const isThisLastActivatedEvent = Boolean(isBefore(new Date(), event.nextEventDate))
     return isThisLastActivatedEvent
   })
-  if (!status) {
+  if (!activeEvent) {
     const whitelistStartDate = timeline[0].date
-    const upcomingStatus = generateUpcomingStatus(whitelistStartDate)
-    return upcomingStatus
+    const upcomingEvent = generateUpcomingEvent(whitelistStartDate)
+    return upcomingEvent
   }
-  return status
+  return activeEvent
 }
