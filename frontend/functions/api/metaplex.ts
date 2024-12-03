e92e72974ecf9c63091e2d9d965e3b0f5fe03412
Strahinja Gojkovic <strahinja.gojkovic@codetri.be>
2024-12-03 12:20:37 +0100

    feat: enabling new metaplex library for minting nft

--- frontend/functions/api/metaplex.ts
+++ frontend/functions/api/metaplex.ts
@@ -1,8 +1,11 @@
 import { jsonResponse, reportError } from "./cfPagesFunctionsUtils"
 import { drizzle } from "drizzle-orm/d1"
 import { keypairIdentity, Metaplex } from "@metaplex-foundation/js"
-import { Connection, Keypair } from "@solana/web3.js"
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction } from "@solana/web3.js"
 import * as bs58 from "bs58"
+import { createNft, createV1, mplTokenMetadata, TokenStandard } from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createSignerFromKeypair, generateSigner, percentAmount, signerIdentity, transactionBuilder, UmiPlugin } from "@metaplex-foundation/umi"
 
 type ENV = {
     DB: D1Database
@@ -12,21 +15,35 @@ export const onRequestGet: PagesFunction<ENV> = async (ctx) => {
     const db = drizzle(ctx.env.DB, { logger: true })
     const rpcUrl = ctx.env.SOLANA_RPC_URL
     try {
-        // insert rpc url
         const connection = new Connection(rpcUrl)
-        // insert your private key for minting nft
-        const myKeypair = Keypair.fromSecretKey(new Uint8Array(bs58.default.decode('')))
+        const umi = createUmi(connection)
 
-        const metaplex = Metaplex.make(connection).use(keypairIdentity(myKeypair))
+        const privateKeypair = Keypair.fromSecretKey(new Uint8Array(bs58.default.decode('5MVdNPqEktnsUdXpZ5x9JMnRXrXuENyjQqNfWMN56kM5rZmbGkrqTS7LyWa2MszcqwgmrMvSygDg8PyitMgJ7shH')))
+        // convert to Umi compatible keypair
+        const umiKeypair = umi.eddsa.createKeypairFromSecretKey(privateKeypair.secretKey)
+        const signer = createSignerFromKeypair(umi, umiKeypair)
+        // create mint address for nft
+        const mint = Keypair.generate()
+        const mintKeypair = umi.eddsa.createKeypairFromSecretKey(mint.secretKey)
+        const mintSigner = createSignerFromKeypair(umi, mintKeypair)
+        umi.use(signerIdentity(signer))
+        umi.use(mplTokenMetadata())
 
-        const response = await metaplex.nfts().create({
-            name: "BorgTestNFT",
-            symbol: "BTNFT",
-            sellerFeeBasisPoints: 0,
-            uri: "https://example.com/json"
-        })
+        const builder = transactionBuilder().add(createNft(umi, {
+            symbol: 'BTNFT',
+            mint: mintSigner,
+            name: "Borg Test NFT",
+            uri: "https://example.com/json",
+            updateAuthority: umi.identity.publicKey,
+            sellerFeeBasisPoints: percentAmount(0),
+        }))
+        const response = await builder.send(umi)
 
-        return jsonResponse({ nftAddress: response.nft.address.toBase58() }, 200)
+        const signature = bs58.default.encode(response)
+        console.log(signature)
+
+
+        return jsonResponse({ transactionId: signature }, 200)
     } catch (e) {
         await reportError(ctx.env.DB, e)
         return jsonResponse({ message: "Something went wrong..." }, 500)
