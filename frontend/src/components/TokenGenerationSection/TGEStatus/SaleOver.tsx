2c58cec49c4d4ae53bea4d7047eda2b8319ed9c3
Vanja <vanja.jaksic@codetri.be>
2024-12-09 17:18:56 +0100

    feat: fix backdrop image full width issue

--- frontend/src/components/TokenGenerationSection/TGEStatus/SaleOver.tsx
+++ frontend/src/components/TokenGenerationSection/TGEStatus/SaleOver.tsx
@@ -62,7 +62,7 @@ const SaleOver = ({ eventData, timeline }: LiveProps) => {
   const hasDistributionStarted = eventData.id === "REWARD_DISTRIBUTION"
 
   return (
-    <div className="flex w-full justify-center px-4">
+    <div className="flex w-full flex-col items-center justify-center px-4">
       <div className="flex w-full max-w-[760px] flex-col items-center">
         <Timeline timelineEvents={timeline} />
 
@@ -89,44 +89,43 @@ const SaleOver = ({ eventData, timeline }: LiveProps) => {
             {tweetId ? <Tweet id={tweetId} /> : <TweetNotFound />}
           </div>
         </div>
-
-        {projectData?.saleData?.saleSucceeded && (
-          <div ref={contributionsRef} className="relative flex w-full flex-col items-center gap-9 px-4 pt-[80px]">
-            <Divider icon="SvgHandWithWallet" />
-            <div
-              className={twMerge(
-                "max-w-screen absolute left-0 top-10 -z-[-10] w-full overflow-hidden lg:top-16",
-                !userDidContribute || walletState !== "CONNECTED" ? "h-[247px] lg:top-0" : "",
-              )}
-            >
-              <img src={backdropImg} className="lg:h-auto lg:w-screen" />
-            </div>
-            <h3 className="px-4 text-[32px] font-semibold leading-tight">{t("sale_over.your_contribution")}</h3>
-            {walletState !== "CONNECTED" ? (
-              <ConnectButton
-                customBtnText={"Connect Wallet to See Contribution"}
-                btnClassName="py-3 px-4 w-full max-w-[400px] text-base z-10"
-              />
-            ) : userDidContribute ? (
-              <>
-                <section className={sectionClass}>
-                  <YourContribution />
-                </section>
-                <section ref={rewardsRef} className={twMerge(sectionClass, "mt-7 gap-4")}>
-                  <Rewards
-                    rewards={contributionInfo.claimPositions.rewards}
-                    hasDistributionStarted={hasDistributionStarted}
-                  />
-                </section>
-              </>
-            ) : (
-              <div className="w-full max-w-[400px] rounded-lg border border-bd-primary bg-secondary px-4 py-3 text-sm opacity-60">
-                {t("sale_over.wallet_didnt_contribute")}
-              </div>
-            )}
-          </div>
-        )}
       </div>
+      {projectData?.saleData?.saleSucceeded && (
+        <div ref={contributionsRef} className="relative flex w-full flex-col items-center gap-9 px-4 pt-[80px]">
+          <Divider icon="SvgHandWithWallet" />
+          <div
+            className={twMerge(
+              "max-w-screen absolute left-0 top-10 -z-[-10] w-full overflow-hidden lg:top-16",
+              !userDidContribute || walletState !== "CONNECTED" ? "h-[247px] lg:top-0" : "",
+            )}
+          >
+            <img src={backdropImg} className="lg:h-auto lg:w-screen" />
+          </div>
+          <h3 className="px-4 text-[32px] font-semibold leading-tight">{t("sale_over.your_contribution")}</h3>
+          {walletState !== "CONNECTED" ? (
+            <ConnectButton
+              customBtnText={"Connect Wallet to See Contribution"}
+              btnClassName="py-3 px-4 w-full max-w-[400px] text-base z-10"
+            />
+          ) : userDidContribute ? (
+            <>
+              <section className={sectionClass}>
+                <YourContribution />
+              </section>
+              <section ref={rewardsRef} className={twMerge(sectionClass, "mt-7 gap-4")}>
+                <Rewards
+                  rewards={contributionInfo.claimPositions.rewards}
+                  hasDistributionStarted={hasDistributionStarted}
+                />
+              </section>
+            </>
+          ) : (
+            <div className="w-full max-w-[400px] rounded-lg border border-bd-primary bg-secondary px-4 py-3 text-sm opacity-60">
+              {t("sale_over.wallet_didnt_contribute")}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
