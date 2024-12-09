9b481706796746c5ca4bb90d9853b8ef5c91c60a
Vanja <vanja.jaksic@codetri.be>
2024-12-09 12:29:22 +0100

    feat: check if wallet disconnected

--- frontend/src/hooks/useWalletContext.tsx
+++ frontend/src/hooks/useWalletContext.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react"
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react"
 import { usePersistedState } from "@/hooks/usePersistedState.ts"
 import { isMobile } from "@/utils/isMobile.ts"
 import { useSearchParams } from "react-router-dom"
@@ -21,7 +15,7 @@ const PAGE_DOMAIN = window.location.host
 
 export type WalletState = "NOT_CONNECTED" | "CONNECTING" | "CONNECTED"
 
-const SupportedWallets = ["PHANTOM", "BACKPACK", 'SOLFLARE'] as const
+const SupportedWallets = ["PHANTOM", "BACKPACK", "SOLFLARE"] as const
 export type SupportedWallet = (typeof SupportedWallets)[number]
 
 type Context = {
@@ -41,8 +35,7 @@ const WalletContext = createContext<Context | undefined>(undefined)
 
 export function useWalletContext() {
   const context = useContext(WalletContext)
-  if (!context)
-    throw new Error("Component is outside of the <WalletProvider />")
+  if (!context) throw new Error("Component is outside of the <WalletProvider />")
   return context
 }
 
@@ -52,7 +45,6 @@ export function useWalletContext() {
  * Typing not complete.
  */
 export type Provider = {
-
   signIn: (args: { domain: string }) => Promise<{
     address: { toString(): string }
   }>
@@ -65,9 +57,9 @@ export type Provider = {
 }
 
 export type signTransactionArgs = {
-  walletType: SupportedWallet,
-  tokenAmount: number,
-  rpcUrl: string,
+  walletType: SupportedWallet
+  tokenAmount: number
+  rpcUrl: string
   tokenMintAddress: PublicKey
 }
 
@@ -89,14 +81,9 @@ type SignInWithArgs = {
 export function WalletProvider({ children }: { children: ReactNode }) {
   //// hooks
   const [address, setAddress] = usePersistedState("address")
-  const [walletProvider, setWalletProvider] = usePersistedState<
-    SupportedWallet | ""
-  >("wallet")
-  const initialWalletState: WalletState = address
-    ? "CONNECTED"
-    : "NOT_CONNECTED"
-  const [walletState, setWalletState] =
-    useState<WalletState>(initialWalletState)
+  const [walletProvider, setWalletProvider] = usePersistedState<SupportedWallet | "">("wallet")
+  const initialWalletState: WalletState = address ? "CONNECTED" : "NOT_CONNECTED"
+  const [walletState, setWalletState] = useState<WalletState>(initialWalletState)
 
   // autoConnect feature
   const [searchParams, setSearchParams] = useSearchParams()
@@ -108,7 +95,7 @@ export function WalletProvider({ children }: { children: ReactNode }) {
       signInWithPhantom()
     } else if (autoConnect.toUpperCase() === "BACKPACK") {
       signInWithBackpack()
-    } else if (autoConnect.toUpperCase() === 'SOLFLARE') {
+    } else if (autoConnect.toUpperCase() === "SOLFLARE") {
       signInWithSolflare()
     }
 
@@ -174,7 +161,7 @@ export function WalletProvider({ children }: { children: ReactNode }) {
         return address
       },
     })
-}
+  }
 
   async function signInWith({ wallet, getProvider, signIn }: SignInWithArgs) {
     const provider = getProvider()
@@ -186,7 +173,7 @@ export function WalletProvider({ children }: { children: ReactNode }) {
         const url = `${PAGE_URL}/?autoConnect=${wallet}`
         const encodedUrl = encodeURIComponent(url)
         const encodedPageUrl = encodeURIComponent(PAGE_URL)
-        if (wallet === 'SOLFLARE') {
+        if (wallet === "SOLFLARE") {
           const deepLink = `https://${wallet}.com/ul/v1/browse/${encodedUrl}?ref=${encodedPageUrl}`
           window.location.href = deepLink
         } else {
@@ -228,21 +215,21 @@ export function WalletProvider({ children }: { children: ReactNode }) {
   async function signTransaction({ walletType, tokenAmount, rpcUrl, tokenMintAddress }: signTransactionArgs) {
     let provider
     try {
-      if (walletType === 'BACKPACK') {
+      if (walletType === "BACKPACK") {
         // @ts-ignore-next-line
         provider = window?.backpack
         if (!provider.isConnected) {
           toast("Wallet session timed out, please sign in again")
           await signInWithBackpack()
         }
-      } else if (walletType === 'PHANTOM') {
+      } else if (walletType === "PHANTOM") {
         // @ts-ignore-next-line
         provider = window?.phantom.solana
         if (!provider.isConnected) {
           toast("Wallet session timed out, please sign in again")
           await signInWithPhantom()
         }
-      } else if (walletType === 'SOLFLARE') {
+      } else if (walletType === "SOLFLARE") {
         // @ts-ignore-next-line
         provider = window?.solflare
         if (!provider.isConnected) {
@@ -250,7 +237,7 @@ export function WalletProvider({ children }: { children: ReactNode }) {
           await signInWithSolflare()
         }
       }
-      if (!provider) throw new Error('Provider not found!')
+      if (!provider) throw new Error("Provider not found!")
       const txToSend = await getTransactionToSend(tokenAmount, provider, rpcUrl, tokenMintAddress)
 
       return txToSend
@@ -270,15 +257,15 @@ export function WalletProvider({ children }: { children: ReactNode }) {
   }
 
   async function signMessage(message: string) {
-    if (walletProvider === 'PHANTOM') {
+    if (walletProvider === "PHANTOM") {
       // @ts-expect-error
       const signature = await window.solana.signMessage(Buffer.from(message))
       return signature.signature
-    } else if (walletProvider === 'BACKPACK') {
+    } else if (walletProvider === "BACKPACK") {
       // @ts-expect-error
       const signature = await window.backpack.signMessage(Buffer.from(message))
       return signature.signature
-    } else if (walletProvider === 'SOLFLARE') {
+    } else if (walletProvider === "SOLFLARE") {
       // @ts-expect-error
       const signature = await window.solflare.signMessage(Buffer.from(message))
       return signature.signature
@@ -288,6 +275,37 @@ export function WalletProvider({ children }: { children: ReactNode }) {
   }
 
   const truncatedAddress = truncateAddress(address)
+
+  const checkIfUserIsDisconnectedFromWallet = useCallback(
+    (callbackIfDisconnected: () => void) => {
+      if (!walletProvider) return
+      let provider
+      if (walletProvider === "BACKPACK") {
+        // @ts-expect-error types
+        provider = window?.backpack
+        if (!provider.isConnected) {
+          callbackIfDisconnected()
+        }
+      } else if (walletProvider === "PHANTOM") {
+        // @ts-expect-error types
+        provider = window?.phantom.solana
+        if (!provider.isConnected) {
+          callbackIfDisconnected()
+        }
+      } else if (walletProvider === "SOLFLARE") {
+        // @ts-expect-error types
+        provider = window?.solflare
+        if (!provider.isConnected) {
+          callbackIfDisconnected()
+        }
+      }
+    },
+    [walletProvider],
+  )
+
+  useEffect(() => {
+    checkIfUserIsDisconnectedFromWallet(() => setWalletState("NOT_CONNECTED"))
+  }, [checkIfUserIsDisconnectedFromWallet])
 
   return (
     <WalletContext.Provider
