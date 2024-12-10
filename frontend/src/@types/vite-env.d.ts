620746212ee99342e03f6bf898a35bee0f22e236
milan.miljus <milan.miljus@codetri.be>
2024-12-10 10:09:50 +0100

    feat: lbp wallet address in json

--- frontend/src/@types/vite-env.d.ts
+++ frontend/src/@types/vite-env.d.ts
@@ -2,5 +2,4 @@ interface ImportMetaEnv {
   readonly VITE_ENVIRONMENT_TYPE: string
   readonly VITE_TWITTER_CLIENT_ID: string
   readonly VITE_API_BASE_URL: string
-  readonly VITE_LBP_WALLET_ADDRESS: string
 }
