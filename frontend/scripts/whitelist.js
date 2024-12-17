c9d28f0a93c826b3157d54a9c6f88e5195267d23
milan.miljus <milan.miljus@codetri.be>
2024-12-17 13:16:18 +0100

    docs

---
+++ frontend/scripts/whitelist.js
@@ -0,0 +1,33 @@
+
+// {
+//   "rows": [
+//   {
+//     "projectId": "solana-id",
+//     "address": "",
+//     "tierId": "tier10"
+//   }
+// ]
+// }
+
+const projectId = ''
+const tierId = ''
+const input = ``
+
+function main() {
+  const addresses = input
+    .split('\n')
+    .map(s => s.trim())
+    .filter(Boolean)
+    .map(address => ({
+      projectId,
+      tierId,
+      address,
+    }))
+  const req = {
+    rows: addresses
+  }
+
+  console.log(JSON.stringify(req, null, 2))
+}
+
+main()
