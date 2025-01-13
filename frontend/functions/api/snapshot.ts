f78ccc72dc789b4a4d66208c6e5d36db1a03922a
milan.miljus <milan.miljus@codetri.be>
2025-01-13 18:16:19 +0100

    feat: eligibility snapshot function

---
+++ frontend/functions/api/snapshot.ts
@@ -0,0 +1,71 @@
+import { hasAdminAccess, jsonResponse, reportError } from "./cfPagesFunctionsUtils"
+import { z } from "zod"
+import { SnapshotService } from "../services/snapshotService"
+import { drizzle } from "drizzle-orm/d1"
+import { EligibilityService } from "../services/eligibilityService"
+import { projectTable } from "../../shared/drizzle-schema"
+import { eq, sql } from "drizzle-orm"
+import { getRpcUrlForCluster } from "../../shared/solana/rpcUtils"
+
+const requestSchema = z.object({
+  projectId: z.string().min(2),
+  limit: z.number().int().min(1).max(999),
+})
+
+type ENV = {
+  DB: D1Database
+  ADMIN_API_KEY_HASH: string
+}
+export const onRequestPost: PagesFunction<ENV> = async (ctx) => {
+  const db = drizzle(ctx.env.DB, { logger: true })
+
+  try {
+    // authorize request
+    if (!hasAdminAccess(ctx)) {
+      return jsonResponse(null, 401)
+    }
+
+    // parse request
+    const requestJson = await ctx.request.json()
+    const { error, data } = requestSchema.safeParse(requestJson)
+
+    // validate request
+    if (error || !data) {
+      return jsonResponse({ message: "Invalid request!", error }, 400)
+    }
+
+    const { projectId, limit } = data
+
+    const project = await db
+      .select()
+      .from(projectTable)
+      .where(eq(projectTable.id, projectId))
+      .get()
+    if (!project) return jsonResponse({ message: 'Project not found!' }, 404)
+    const rpcUrl = getRpcUrlForCluster(ctx.env.SOLANA_RPC_URL, project.json.config.cluster)
+
+    // load addresses
+    const addresses = (await db.run(sql`SELECT address FROM user WHERE address NOT IN (SELECT address FROM eligibility_status_snapshot WHERE project_id = ${projectId})`))
+      .results as string[]
+
+    const paginatedAddresses = addresses.slice(0, limit)
+
+    for (const address of paginatedAddresses) {
+      const existingSnapshot = await SnapshotService.getSnapshot({ db, address, projectId })
+      if (existingSnapshot) continue
+
+      const eligibilityStatus = await EligibilityService.getEligibilityStatus({ db, address, projectId, rpcUrl })
+      await SnapshotService.createSnapshot({ db, address, projectId, eligibilityStatus })
+    }
+
+    return jsonResponse({
+      message: "Ok",
+      doneCount: limit,
+      todoCount: addresses.length - limit,
+    }, 200)
+  } catch (e) {
+    const db = ctx.env.DB
+    await reportError(db, e)
+    return jsonResponse({ message: "Something went wrong..." }, 500)
+  }
+}
