e21c8db2479d8647ed22e0d01b63a99565fd7b45
Vanja <vanja.jaksic@codetri.be>
2025-02-11 14:34:00 +0100

    feat: update cloud_load_test.js

--- frontend/scripts/cloud_load_test.js
+++ frontend/scripts/cloud_load_test.js
@@ -1,4 +1,3 @@
-import { sleep } from "k6"
 import http from "k6/http"
 
 export const options = {
@@ -23,28 +22,32 @@ export const options = {
   },
 }
 
-export function scenario_1() {
-  const projectId = "borgy"
-  const baseUrl = "https://borgpad.com/api"
-  const address = "4GvgisWbCKJCFfksnU44qyRAVwd8YjxuhhsDCDSRjMnL" // test address
+// LOAD CONFIG
+const cluster = "mainnet"
+const projectId = "hidden-agentlauncher-test-9891843819"
+const baseUrl = "https://borgpad.com/api"
+const address = "4GvgisWbCKJCFfksnU44qyRAVwd8YjxuhhsDCDSRjMnL" // test address
+const raisedTokenDataName = "usd"
 
+export function scenario_1() {
   // GET Requests
   http.batch([
+    `${baseUrl}/rpcproxy?cluster=${cluster}`,
     `${baseUrl}/investmentintentsummary?projectId=${projectId}`,
-    `${baseUrl}/exchange?baseCurrency=swissborg&targetCurrency=usd`,
+    `${baseUrl}/exchange?baseCurrency=${raisedTokenDataName}&targetCurrency=usd`,
     `${baseUrl}/saleresults?projectId=${projectId}`,
     `${baseUrl}/deposits?address=${address}&projectId=${projectId}`,
     `${baseUrl}/depositstatus?address=${address}&projectId=${projectId}`,
     `${baseUrl}/eligibilitystatus?address=${address}&projectId=${projectId}`,
   ])
+  // POST requests example
+  const body1 = {
+    userWalletAddress: address,
+    projectId,
+    tokenAmount: 50,
+  }
+  http.post("https://borgpad.com/api/createdeposittransaction", JSON.stringify(body1), {
+    headers: { "Content-Type": "application/json" },
+  })
 }
 
-// POST requests
-// const body1 = {
-//   userWalletAddress: "Ee5sD8JhRqLoL8rJxZG6Yf1ehHGUks91wek9nCoWUX43",
-//   projectId,
-//   tokenAmount: 50,
-// }
-// http.post("https://borgpad.com/api/createdeposittransaction", JSON.stringify(body1), {
-//   headers: { "Content-Type": "application/json" },
-// })
