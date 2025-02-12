9c5282124d2b15f34aa7c0306fdd0aa15b0d1bd7
milan.miljus <milan.miljus@codetri.be>
2025-02-12 10:08:41 +0100

    feat: export script

--- frontend/scripts/export.bash
+++ frontend/scripts/export.bash
@@ -30,3 +30,23 @@ npx wrangler d1 execute borgpad-production-database --remote --command "SELECT
   json -> 'referral' -> 'borgy' ->> 'referrerTwitterHandle' AS referrer
 FROM user
 WHERE json -> 'investmentIntent' -> 'borgy' IS NOT NULL;" &> "export_borgy_$(date -u +%Y%m%d_%H%M%S).json"
+
+# zkagi
+npx wrangler d1 execute borgpad-production-database --remote --command "SELECT
+  address,
+  json -> 'investmentIntent' -> 'zkagi' ->> 'amount' AS investment_interest_amount,
+  json -> 'investmentIntent' -> 'zkagi' ->> 'providedAt' AS investment_interest_provided_at,
+  json -> 'termsOfUse' ->> 'countryOfOrigin' AS terms_of_use_country_of_origin,
+  json -> 'termsOfUse' ->> 'acceptedAt' AS terms_of_use_accepted_at
+FROM user
+WHERE json -> 'investmentIntent' -> 'zkagi' IS NOT NULL;" &> "export_zkagi_$(date -u +%Y%m%d_%H%M%S).json"
+
+# agentlauncher
+npx wrangler d1 execute borgpad-production-database --remote --command "SELECT
+  address,
+  json -> 'investmentIntent' -> 'agentlauncher' ->> 'amount' AS investment_interest_amount,
+  json -> 'investmentIntent' -> 'agentlauncher' ->> 'providedAt' AS investment_interest_provided_at,
+  json -> 'termsOfUse' ->> 'countryOfOrigin' AS terms_of_use_country_of_origin,
+  json -> 'termsOfUse' ->> 'acceptedAt' AS terms_of_use_accepted_at
+FROM user
+WHERE json -> 'investmentIntent' -> 'agentlauncher' IS NOT NULL;" &> "export_agentlauncher_$(date -u +%Y%m%d_%H%M%S).json"
