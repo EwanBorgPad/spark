c653a3f2771699c9293c16f87236fc12e533952f
Vanja <vanja.jaksic@codetri.be>
2025-02-27 15:39:05 +0100

    fix: discover cards mobile view fix

--- frontend/src/components/LandingPage/DiscoverSection.tsx
+++ frontend/src/components/LandingPage/DiscoverSection.tsx
@@ -8,8 +8,8 @@ import discoverDraftPicksMd from "@/assets/landingPage/discover-draft-picks.png"
 import discoverBlitzPoolsMd from "@/assets/landingPage/discover-blitz-pools.png"
 import discoverGoatPoolsMd from "@/assets/landingPage/discover-goat-pools.png"
 import discoverDraftPicksSmall from "@/assets/launchPools/draft-picks-logo.png"
-import discoverBlitzLogoSmall from "@/assets/landingPage/bolt-logo-small.png"
-import discoverGoatLogoSmall from "@/assets/landingPage/goat-logo-small.png"
+import discoverBlitzLogoSmall from "@/assets/launchPools/blitz-pools-logo.png"
+import discoverGoatLogoSmall from "@/assets/launchPools/goat-pools-logo.png"
 import backdropDraft from "@/assets/landingPage/discover-draft-backdrop.png"
 import backdropGoat from "@/assets/landingPage/discover-goat-backdrop.png"
 import backdropBlitz from "@/assets/landingPage/discover-blitz-backdrop.png"
@@ -30,10 +30,10 @@ const content: DiscoverSectionCardType[][] = [
   [
     {
       path: ROUTES.DRAFT_PICKS,
-      label: "Goat Pools",
+      label: "Draft Picks",
       description: "Express your investment interest in the up-and-coming talent.",
       images: { small: discoverDraftPicksSmall, medium: discoverDraftPicksMd },
-      imgClass: "h-[116px] w-fit",
+      imgClass: "md:h-[116px] w-[170px] md:w-fit",
       backdropImg: backdropDraft,
       btnClass: "explore-draft-picks",
       className: "discover-draft-picks",
@@ -43,7 +43,7 @@ const content: DiscoverSectionCardType[][] = [
       label: "Blitz Pools",
       description: "Blazing fast pools with electric return opportunities",
       images: { small: discoverBlitzLogoSmall, medium: discoverBlitzPoolsMd },
-      imgClass: "h-[116px] w-fit",
+      imgClass: "md:h-[116px] w-[170px] md:w-fit",
       backdropImg: backdropBlitz,
       btnClass: "explore-blitz-pools",
       className: "discover-blitz-pools",
@@ -53,7 +53,7 @@ const content: DiscoverSectionCardType[][] = [
       label: "Goat Pools",
       description: "Provide Liquidity, Earn Tokens at Pre-TGE Valuations.",
       images: { small: discoverGoatLogoSmall, medium: discoverGoatPoolsMd },
-      imgClass: "h-[116px] w-fit",
+      imgClass: "md:h-[116px] w-[170px] md:w-fit",
       backdropImg: backdropGoat,
       btnClass: "explore-goat-pools",
       className: "discover-goat-pools",
@@ -65,14 +65,14 @@ const content: DiscoverSectionCardType[][] = [
       label: "",
       description: "Stake $SOL, Receive Airdrops from Early Investments.",
       images: { small: discover1, medium: discover1 },
-      imgClass: "max-w-[240px] py-10 mb-0",
+      imgClass: "max-w-[240px] md:py-10 mb-0",
     },
     {
       path: "",
       label: "",
       description: "Get Early Access to Exclusive Token Sales.",
       images: { small: discover2, medium: discover2 },
-      imgClass: "max-w-[240px] py-10 mb-0",
+      imgClass: "max-w-[240px] md:py-10 mb-0",
     },
   ],
 ]
@@ -82,7 +82,11 @@ const DiscoverSection = () => {
 
   return (
     <section className="flex w-full flex-col items-center gap-6 px-5 py-16 md:gap-[60px] md:px-16 md:py-10">
-      <h2 className="text-center text-2xl font-semibold leading-[44px]">Discover the BetterThanCEX Ecosystem</h2>
+      <div className="w-full">
+        <h2 className="w-full max-w-[200px] text-left text-2xl font-semibold leading-[44px] md:max-w-[100%] md:text-center">
+          Discover the BetterThanCEX Ecosystem
+        </h2>
+      </div>
       <div ref={ref} className={twMerge("grid w-full max-w-[1150px] grid-cols-1 gap-6 md:grid-cols-3 md:gap-8")}>
         {content[0].map((item, index) => (
           <DiscoverSectionCard key={index} {...item} />
