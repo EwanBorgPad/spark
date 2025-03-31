68e5b8c9b2d6ec96b8fca59e66f027971c779531
Vanja <vanja.jaksic@codetri.be>
2025-03-31 10:43:00 +0200

    feat: update copy for 100% unlock at tge

--- frontend/src/utils/reward-helper.ts
+++ frontend/src/utils/reward-helper.ts
@@ -6,6 +6,8 @@ export const getRewardDistributionMessage = (project: ProjectModel) => {
 
   const fractionPerMonthAfterTge = rewardDistribution.afterTge.rewardRatio / rewardDistribution.afterTge.numberOfPayments
 
+  if (rewardDistribution.afterTge.numberOfPayments === 0) return "100% unlock at TGE"
+
   return `1/${1/rewardDistribution.atTge.rewardRatio} at TGE and 1/${1/fractionPerMonthAfterTge} for ${rewardDistribution.afterTge.numberOfPayments} months`
 }
 
