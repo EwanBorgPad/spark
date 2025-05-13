5e4157c49f9faa3eb36b79efee33815ef2ac4ddb
Ewan Hamon <ewan.hamon@swissborg.com>
2025-05-13 13:52:54 +0400

    fix: :bug: fix rpcurl error on stage

--- frontend/functions/api/rpcproxy.ts
+++ frontend/functions/api/rpcproxy.ts
@@ -3,6 +3,7 @@ import { jsonResponse, reportError } from "./cfPagesFunctionsUtils"
 import { getRpcUrlForCluster } from "../../shared/solana/rpcUtils"
 
 const allowedMethods = [
+  "simulateTransaction",
   "getMultipleAccounts",
   "getAccountInfo",
   "getTokenAccountsByOwner",
