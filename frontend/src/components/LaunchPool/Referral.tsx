368443dee3e2dd8e11e1704b27d1fe7a284cd1fc
Ewan Hamon <ewan.hamon@swissborg.com>
2025-05-16 09:24:34 +0400

    feat: :lipstick: Update the button Refer when referral has started

--- frontend/src/components/LaunchPool/Referral.tsx
+++ frontend/src/components/LaunchPool/Referral.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
 import { twMerge } from "tailwind-merge"
+import { useWalletContext } from "@/hooks/useWalletContext"
+import { useParams } from "react-router-dom"
 
 import { Icon } from "../Icon/Icon"
 import { Button } from "../Button/Button"
 import ReferralModal from "../Modal/Modals/ReferralModal"
+import { useQuery } from "@tanstack/react-query"
+import { referralApi } from "@/data/api/referralApi"
+import { SvgPodium } from "../Icon/Svg/SvgPodium"
 
 type Props = {
   className?: string
@@ -11,6 +16,26 @@ type Props = {
 
 const Referral = ({ className }: Props) => {
   const [showModal, setShowModal] = useState(false)
+  const { address } = useWalletContext()
+  const { projectId } = useParams()
+  
+  const { data: leaderboardData } = useQuery({
+    queryKey: ["getLeaderboard", address],
+    queryFn: () => referralApi.getLeaderboard({ projectId: projectId || "" }),
+  })
+
+  const userEntry = address && leaderboardData?.leaderboardReferrals
+    ? leaderboardData.leaderboardReferrals.find(item => item.referrer_by === address.substring(0, 4))
+    : null
+
+  const tickets = userEntry?.total_invested || 0
+  const userRanking = address && leaderboardData?.leaderboardReferrals
+    ? leaderboardData.leaderboardReferrals.findIndex(item => item.referrer_by === address.substring(0, 4)) + 1 
+    : 0
+
+  const btnText = tickets !== 0 
+    ? ``
+    : "Refer & Earn"
 
   return (
     <>
@@ -19,10 +44,21 @@ const Referral = ({ className }: Props) => {
         onClick={() => setShowModal(true)}
       >
         <Button
-          btnText="Refer & Earn"
+          btnText={btnText}
           color="secondary"
-          className="text-sm text-fg-brand-primary"
-          prefixElement={<Icon icon="SvgTicket" className="text-xl text-fg-secondary" />}
+          className="text-sm text-fg-brand-primary bg-gradient-to-r from-[#ACFF731A] to-transparent"
+          prefixElement={
+            <div className="flex items-center gap-2">
+              <Icon icon="SvgTicket" className="text-xl text-fg-secondary" />
+              {tickets !== 0 && (
+                <>
+                  <span className="text-fg-brand-primary">{tickets}</span>
+                  <SvgPodium className="text-fg-primary" />
+                  <span className="text-fg-primary">{userRanking}</span>
+                </>
+              )}
+            </div>
+          }
         />
       </div>
 
